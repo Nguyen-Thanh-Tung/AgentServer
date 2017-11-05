4289820e6bc58d1be9c470413dde984239fe859c
Nguyen Thanh Tung <thanhtung.tvg95@gmail.com>
2017-11-05 10:53:47 +0700

    Update

--- constants/constants.js
+++ constants/constants.js
@@ -1,9 +1,9 @@
 const constants = {
-  numberServer: 20,
+  numberServer: 0,
   commandTest: 'loadtest -c 1 -t 10 http://127.0.0.1:',
   preNameServer: 'ws://',
   nameServer: 'localhost:',
-  ipServer: '192.168.1.209',
+  ipServer: '127.0.0.1',
   portServer: '8000',
   error: {
     connectDB: 'Error when connect db',
